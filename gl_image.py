1514f872424bcd64571565d024de07d53b99a8dd
zzzzzhy <11657924@qq.com>
2022-09-30 23:22:33 +0800

    test

--- gl_image.py
+++ gl_image.py
@@ -287,7 +287,6 @@ def create_files(im_path, board, ver, dev_type):
 
 def download_custom_ipk(imageName, path,model):
     import shutil
-    shutil.move(os.getcwd()+'/custom',)
     for root, dirs, files in os.walk(os.getcwd()+'/model'):
         for file in files:
             src_file = os.path.join(root, file)
